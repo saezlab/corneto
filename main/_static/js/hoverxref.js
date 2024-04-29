ab47faff4a138738b0847cf2483a13e2e9d8f55c
pablormier <pablormier@users.noreply.github.com>
2024-04-29 18:39:32 +0000

    deploy: 5fa9d32257c258715b7846d4cbd68eb3d020751b

--- main/_static/js/hoverxref.js
+++ main/_static/js/hoverxref.js
@@ -82,7 +82,7 @@ function reLoadSphinxTabs() {
 function getEmbedURL(url) {
     var params = {
         'doctool': 'sphinx',
-        'doctoolversion': '5.3.0',
+        'doctoolversion': '7.3.7',
         'url': url,
     }
     console.debug('Data: ' + JSON.stringify(params));
